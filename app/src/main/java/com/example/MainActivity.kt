fd7b8fef43b3707cfa1072c20575b22394ff2c23
Vitalik-M07 <vitalik.mahovik@gmail.com>
2023-06-10 17:32:09 +0300

    Прошел эту лабораторию до конца. Добавил действия к кнопке и вывод результата. https://developer.android.com/courses/pathways/android-basics-kotlin-unit-2-pathway-1

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{calculateTip()}
     }
+
+     private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+         val cost = stringInTextField.toDoubleOrNull()
+         if(cost ==null|| cost == 0.0){
+            displayTip(0.0)
+             return
+         }
+         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+             R.id.option_twenty_percent -> 0.20
+             R.id.option_eighteen_percent -> 0.18
+             else -> 0.15
+
+         }
+         var tip = tipPercentage * cost
+
+         if (binding.roundUpSwitch.isChecked){
+             tip = kotlin.math.ceil(tip)
+         }
+         displayTip(tip)
+
+
+    }
+    private fun displayTip(tip: Double){
+        val formatedTip = NumberFormat.getCurrencyInstance().format(0.0)
+        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
+    }
+
+
+
+
+
 }
